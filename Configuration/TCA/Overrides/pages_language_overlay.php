f0bc3401f1a1bc7d1f43bf5a6a9e380ea94e36bd
Scheidler, Nicolas <Nicolas.Scheidler@ecom-ex.com>
2015-03-24 15:56:29 +0100

    Cleanup old TCA fields

--- Configuration/TCA/Overrides/pages_language_overlay.php
+++ Configuration/TCA/Overrides/pages_language_overlay.php
@@ -2,7 +2,7 @@
 require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('sz_pagesettings') . 'Resources/Private/PHP/NewPagesColumns.php');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages_language_overlay', $newPagesColumns, 1);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages_language_overlay', '--div--;SZ Pagesettings, tx_szpagesettings_hideFooterDesc, tx_szpagesettings_footerDesc;;;richtext:rte_transform[flag=rte_enabled|mode=ts], tx_szpagesettings_productImgFal, tx_szpagesettings_productImg, tx_szpagesettings_productCancelled, tx_szpagesettings_productZone, tx_szpagesettings_productDivision,--div--;SZ Teaser;,tx_szpagesettings_teaserHeadline, tx_szpagesettings_teaserTxt;;;richtext:rte_transform[flag=rte_enabled|mode=ts], tx_szpagesettings_teaserImgFal, tx_szpagesettings_teaserImg, tx_szpagesettings_teaserLink, tx_szpagesettings_teaserLinktxt,tx_szpagesettings_pageIcon');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages_language_overlay', '--div--;SZ Pagesettings, tx_szpagesettings_hideFooterDesc, tx_szpagesettings_footerDesc;;;richtext:rte_transform[flag=rte_enabled|mode=ts], tx_szpagesettings_productImgFal, tx_szpagesettings_productCancelled, tx_szpagesettings_productZone, tx_szpagesettings_productDivision,--div--;SZ Teaser;,tx_szpagesettings_teaserHeadline, tx_szpagesettings_teaserTxt;;;richtext:rte_transform[flag=rte_enabled|mode=ts], tx_szpagesettings_teaserImgFal, tx_szpagesettings_teaserLink, tx_szpagesettings_teaserLinktxt,tx_szpagesettings_pageIcon');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
 	'pages_language_overlay',
