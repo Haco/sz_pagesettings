56ee0634191c4396830fc8c875fcea13bbbd24ea
Scheidler, Nicolas <Nicolas.Scheidler@ecom-ex.com>
2015-03-24 14:50:51 +0100

    Update

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,5 +3,5 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['FE']['pageOverlayFields'] .= ',tx_szpagesettings_teaserTxt,tx_szpagesettings_teaserHeadline,tx_szpagesettings_teaserImg,tx_szpagesettings_teaserImgFal,tx_szpagesettings_productImg,tx_szpagesettings_productImgFal,tx_szpagesettings_teaserLink,subnav_title,tx_szpagesettings_teaserLinktxt,tx_szpagesettings_pageIcon,tx_szpagesettings_footerDesc';
-$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',tx_szpagesettings_teaserTxt,tx_szpagesettings_teaserHeadline,tx_szpagesettings_teaserImg,tx_szpagesettings_teaserImgFal,tx_szpagesettings_productImg,tx_szpagesettings_productImgFal,tx_szpagesettings_teaserLink,subnav_title,tx_szpagesettings_teaserLinktxt,tx_szpagesettings_pageIcon,tx_szpagesettings_footerDesc';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['pageOverlayFields'] .= ',tx_szpagesettings_teaserTxt,tx_szpagesettings_teaserHeadline,tx_szpagesettings_teaserImg,tx_szpagesettings_teaserImgFal,tx_szpagesettings_productImgFal,tx_szpagesettings_teaserLink,subnav_title,tx_szpagesettings_teaserLinktxt,tx_szpagesettings_pageIcon,tx_szpagesettings_footerDesc';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',tx_szpagesettings_teaserTxt,tx_szpagesettings_teaserHeadline,tx_szpagesettings_teaserImg,tx_szpagesettings_teaserImgFal,tx_szpagesettings_productImgFal,tx_szpagesettings_teaserLink,subnav_title,tx_szpagesettings_teaserLinktxt,tx_szpagesettings_pageIcon,tx_szpagesettings_footerDesc';
